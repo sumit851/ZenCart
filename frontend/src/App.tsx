03e1bc89a67adadfde94f292d20f1ca57aea90ce
Sumit Dalai <dalaisumit611@gmail.com>
2024-11-23 20:16:02 +0530

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+const Search = lazy(() => import("./pages/Search"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Home = lazy(() => import("./pages/Home"));
+
+//Admin routes importing
+
+const Dashboard = lazy(() => import("./pages/admin/dashboard"));
+const Products = lazy(() => import("./pages/admin/products"));
+const Customers = lazy(() => import("./pages/admin/customers"));
+const Transaction = lazy(() => import("./pages/admin/transaction"));
+const Barcharts = lazy(() => import("./pages/admin/charts/barcharts"));
+const Piecharts = lazy(() => import("./pages/admin/charts/piecharts"));
+const Linecharts = lazy(() => import("./pages/admin/charts/linecharts"));
+const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
+const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
+const Toss = lazy(() => import("./pages/admin/apps/toss"));
+const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
+const ProductManagement = lazy(
+  () => import("./pages/admin/management/productmanagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/admin/management/transactionmanagement")
+);
+
+function App() {
+  return (
+    <Router>
+      {/* Header */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
